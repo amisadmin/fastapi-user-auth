41406844e0ab370218b9c9be485e7daaa9bd0c72
625412505 <82276246+625412505@users.noreply.github.com>
2022-03-20 10:05:40 +0800

    style：header增加个人信息和退出登录样式

--- fastapi_user_auth/site.py
+++ fastapi_user_auth/site.py
@@ -1,4 +1,6 @@
+import fastapi_amis_admin
 from fastapi import FastAPI
+from fastapi_amis_admin.amis.components import Flex, Tpl
 from fastapi_amis_admin.amis_admin.settings import Settings
 from fastapi_amis_admin.amis_admin.site import AdminSite
 from sqlalchemy.ext.asyncio import AsyncEngine
@@ -21,6 +23,37 @@ class AuthAdminSite(AdminSite):
         self.auth = auth or self.auth or Auth(db=self.db)
         UserAuthApp.auth = self.auth
         self.register_admin(UserAuthApp)
+    
+    async def get_page(self, request: Request) -> App:
+        app = await super().get_page(request)
+        user_items = {
+            "type": "dropdown-button",
+            "label": f"{request.user.username}",
+            "trigger": "hover",
+            "icon": "fa fa-user",
+            "buttons": [
+                {
+                    "type": "button",
+                    "label": "个人信息"
+                },
+                {
+                    "type": "button",
+                    "label": "退出登录"
+                }
+            ]
+        }
+        copyright_info = Tpl(style={"margin-left": 5},
+                             tpl=f"""
+                                     <div class="flex justify-between">
+                                        <div>
+                                            <a href="{fastapi_amis_admin.__url__}" target="_blank"'title="版权信息,不可删除!">
+                                                <i class="fa fa-github fa-2x"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                  """
+                             )
+        app.header = Flex(className="w-full", justify='flex-end', alignItems='flex-end', items=[user_items, copyright_info])
 
     async def has_page_permission(self, request: Request) -> bool:
         return await self.auth.requires(response=False)(request)
