a1227c6053bd7e0ef5bcf52b5d41fb8074fb3773
atomi <1456417373@qq.com>
2022-09-23 21:23:05 +0800

    test: Run lint

--- tests/test_auth/test_auth_user_model.py
+++ tests/test_auth/test_auth_user_model.py
@@ -1,21 +1,20 @@
 from datetime import date
 
 import pytest
-from devtools import debug
-from fastapi import Depends, FastAPI
+from fastapi import Depends
 from fastapi_amis_admin.models.fields import Field
-from starlette.testclient import TestClient
 
-from fastapi_user_auth.auth import Auth, AuthRouter
-from fastapi_user_auth.auth.models import BaseUser, User
+from fastapi_user_auth.auth.models import User
 
-# 自定义`User`模型,继承`BaseUser`
+
+# 自定义`User`模型,继承`User`
 class MyUser(User, table=True):
-    point: float = Field(default=0, title='积分', description='用户积分')
-    phone: str = Field(None, title='手机号', max_length=15)
+    point: float = Field(default=0, title="积分", description="用户积分")
+    phone: str = Field(None, title="手机号", max_length=15)
     parent_id: int = Field(None, title="上级", foreign_key="auth_user.id")
     birthday: date = Field(None, title="出生日期")
     location: str = Field(None, title="位置")
+
 
 @pytest.mark.parametrize("logins", ["admin"], indirect=True)
 async def test_custom_user_model(fake_auth, logins):
@@ -25,16 +24,15 @@ async def test_custom_user_model(fake_auth, logins):
     client = logins.client
 
     #  注册auth用户管理路由
-    @app.get('/get_user')
+    @app.get("/get_user")
     async def get_user(user: MyUser = Depends(fake_auth.get_current_user)):
         assert isinstance(user, MyUser)
         user.point = 100
-        user.phone = '123456789'
+        user.phone = "123456789"
         return user
 
-    response = client.get('/get_user')
+    response = client.get("/get_user")
     data = response.json()
-    assert 'parent_id' in data
-    assert data['point'] == 100
-    assert data['phone'] == '123456789'
-
+    assert "parent_id" in data
+    assert data["point"] == 100
+    assert data["phone"] == "123456789"
