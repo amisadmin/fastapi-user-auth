ee4d4acb5bf41b6cf887864dda774fed5ad28cca
atomi <1456417373@qq.com>
2023-08-05 15:38:31 +0800

    perf: Use Path instead of `os.path`

--- fastapi_user_auth/__init__.py
+++ fastapi_user_auth/__init__.py
@@ -3,11 +3,18 @@ __url__ = "https://github.com/amisadmin/fastapi_user_auth"
 
 import gettext
 import os
+from pathlib import Path
 
 from fastapi_amis_admin import i18n
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent
 
 i18n.load_translations(
-    {"zh_CN": gettext.translation(domain="messages", localedir=os.path.join(BASE_DIR, "locale"), languages=["zh_CN"])}
+    {
+        "zh_CN": gettext.translation(
+            domain="messages",
+            localedir=BASE_DIR / "locale",
+            languages=["zh_CN"],
+        ),
+    }
 )
