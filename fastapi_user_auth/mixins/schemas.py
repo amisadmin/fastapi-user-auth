a573207229d89a13ab4600889e412025b0dab8f7
atomi <1456417373@qq.com>
2023-08-30 11:04:09 +0800

    perf: 支持设置用户权限验证属性名称

--- fastapi_user_auth/mixins/schemas.py
+++ fastapi_user_auth/mixins/schemas.py
@@ -54,13 +54,14 @@ class UserSelectPerm(SelectPerm):
     """所属用户选择数据集,只能选择匹配当前用户的数据"""
 
     user_column: str = "user_id"
+    user_attr: str = "id"
 
     async def _call(self, admin: ModelAdmin, request: Request, sel: Select) -> Select:
-        user_id = await admin.site.auth.get_current_user_identity(request, name="id")
-        if not user_id:  # 未登录
+        user = await admin.site.auth.get_current_user(request)
+        if not user:  # 未登录
             return sel.where(False)
         column = getattr(admin.model, self.user_column)
-        return sel.where(column == user_id)
+        return sel.where(column == getattr(user, self.user_attr))
 
 
 @dataclass
