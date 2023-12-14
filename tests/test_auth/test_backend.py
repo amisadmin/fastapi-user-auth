324bff51d31778df5878e2bc0fdac5682bfa7fe2
atomi <1456417373@qq.com>
2023-12-14 21:01:43 +0800

    更新依赖

--- tests/test_auth/test_backend.py
+++ tests/test_auth/test_backend.py
@@ -7,7 +7,6 @@ from fastapi_user_auth.auth.schemas import BaseTokenData
 token_data = BaseTokenData(id=1, username="test")
 
 
-@pytest.mark.asyncio
 async def test_jwt_token_store():
     store = JwtTokenStore(secret_key="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
     token = await store.write_token(token_data)
@@ -18,7 +17,6 @@ async def test_jwt_token_store():
         await store.destroy_token(token)
 
 
-@pytest.mark.asyncio
 async def test_db_token_store(db):
     store = DbTokenStore(db)
     token = await store.write_token(token_data)
@@ -32,6 +30,5 @@ async def test_db_token_store(db):
     assert data is None
 
 
-@pytest.mark.asyncio
 async def test_redis_token_store():
     pass
