e530d7364eb158c5daf64ee63d53247fb461274d
mkadir <muhammadalihakimov03@gmail.com>
2024-03-22 18:51:39 +0500

    exceptions translated to english

--- fastapi_user_auth/auth/exceptions.py
+++ fastapi_user_auth/auth/exceptions.py
@@ -8,36 +8,36 @@ from fastapi_amis_admin.models import IntegerChoices
 class ErrorCode(IntegerChoices):
     """常用错误码"""
 
-    SUCCESS = (0, "成功")
-    FAIL = (1, "失败")
-    PARAMS_ERROR = (2, "参数错误")
-    RETRY = (10, "重试")
-    RETRY_LATER = (11, "稍后重试")
+    SUCCESS = (0, "Success")  # 成功
+    FAIL = (1, "Fail")  # 失败
+    PARAMS_ERROR = (2, "Parameter error")  # 参数错误
+    RETRY = (10, "Retry")  # 重试
+    RETRY_LATER = (11, "Try again later")  # 稍后重试
     # 用户相关错误
-    USER_NOT_FOUND = (40100, "用户不存在")
-    USER_PASSWORD_ERROR = (40101, "用户名或者密码错误")
-    USER_IS_EXIST = (40102, "用户已存在")
-    USER_NAME_IS_EXIST = (40103, "用户名已存在")
-    USER_MOBILE_IS_EXIST = (40104, "用户手机号已存在")
-    USER_EMAIL_IS_EXIST = (40105, "用户邮箱已存在")
+    USER_NOT_FOUND = (40100, "User does not exist")  # 用户不存在
+    USER_PASSWORD_ERROR = (40101, "Username or password is incorrect")  # 用户名或者密码错误
+    USER_IS_EXIST = (40102, "User already exists")  # 用户已存在
+    USER_NAME_IS_EXIST = (40103, "Username already exists")  # 用户名已存在
+    USER_MOBILE_IS_EXIST = (40104, "User mobile phone number already exists")  # 用户手机号已存在
+    USER_EMAIL_IS_EXIST = (40105, "User mailbox already exists")  # 用户邮箱已存在
 
     # 用户权限相关
-    USER_IS_NOT_LOGIN = (40200, "用户未登录")
-    USER_IS_NOT_ACTIVE = (40201, "用户未激活")
-    USER_PERMISSION_DENIED = (40203, "用户权限不足")
-    USER_IS_NOT_ADMIN = (40204, "用户不是管理员")
+    USER_IS_NOT_LOGIN = (40200, "User is not logged in")  # 用户未登录
+    USER_IS_NOT_ACTIVE = (40201, "User is not activated")  # 用户未激活
+    USER_PERMISSION_DENIED = (40203, "Insufficient user rights")  # 用户权限不足
+    USER_IS_NOT_ADMIN = (40204, "User is not an administrator")  # 用户不是管理员
     # 系统错误
-    SYSTEM_ERROR = (50000, "系统错误")
-    SYSTEM_BUSY = (50001, "系统繁忙")
+    SYSTEM_ERROR = (50000, "System error")  # 系统错误
+    SYSTEM_BUSY = (50001, "The system is busy")  # 系统繁忙
 
 
 class ApiException(HTTPException):
     def __init__(
-        self,
-        detail: Any,
-        status_code: int = 200,
-        content: Any = None,
-        headers: Optional[Dict[str, Any]] = None,
+            self,
+            detail: Any,
+            status_code: int = 200,
+            content: Any = None,
+            headers: Optional[Dict[str, Any]] = None,
     ) -> None:
         self.content = content
         super().__init__(status_code=status_code, detail=detail, headers=headers)
@@ -47,11 +47,11 @@ class ApiError(ApiException):
     """API异常基类"""
 
     def __init__(
-        self,
-        status: int = ErrorCode.FAIL,
-        msg: str = "",
-        headers: Optional[Dict[str, Any]] = None,
-        **extra,
+            self,
+            status: int = ErrorCode.FAIL,
+            msg: str = "",
+            headers: Optional[Dict[str, Any]] = None,
+            **extra,
     ):
         self.status = status
         self.extra = extra
